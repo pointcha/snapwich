6e0fa180f4976d7b16202fd2d171c306df2132d0
pointcha <info@pointcha.ca>
2015-11-24 22:35:26 -0400

    challenge and item working

--- scripts/controllers/challenge.js
+++ scripts/controllers/challenge.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('ChallengeController', function($scope, FURL, $firebase, $location, Challenge) {
+app.controller('ChallengeController', function($scope, FURL, $firebase, $location, Challenge, toaster) {
 
 	var ref = new Firebase(FURL);
 	var fbChallenges = $firebase(ref.child('challenge')).$asArray();
@@ -10,38 +10,34 @@ app.controller('ChallengeController', function($scope, FURL, $firebase, $locatio
 
 // generate unique code
 
-		var iCode = randomString(7, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-		while (codeExists(iCode)) {
-			iCode = randomString(7, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-		}
 
-		$scope.challenge.status = 'ready';
-		$scope.challenge.code = iCode;
+var iCode = randomString(7, '23456789abcdefghjkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ');
 
-		//$scope.challenge.name = Auth.user.profile.name;
-		//$scope.challenge.poster = Auth.user.uid;
+$scope.challenge.status = 'ready';
+$scope.challenge.code = iCode;
+$scope.challenge.itemNum = challenge.itemNumber;
 
-		Challenge.createChallenge($scope.challenge).then(function(ref) {
-			$scope.challenge = {title: '', code: '', status: 'open', host: '', leader: '', name: ''};
-			$location.path('/admin/');
+			//$scope.challenge = {title: '', code: '', status: 'open', host: '', leader: '', name: ''};
+
+			checkForCode(challenge.code, function(doesExist) {
+
+				if (doesExist) {
+				// already exists
+				toaster.pop('warning', "Code already exists");
+			} else {
+				// does not exist
+				ref.child('challenge').child(challenge.code).set({ title: challenge.title, code: challenge.code, itemNum: challenge.itemNumber, status: challenge.status});
+				toaster.pop('success', "Challenge created");
+				$location.path('/admin/');
+			}
 		});
-	};
 
-	function randomString(length, chars) {
-		var result = '';
-		for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-			return result;
-	}
+		};
 
-	function codeExists(iCode) {
-		var existsFlag = false;
-
-		for (var r = 0; 1 < $scope.challenge.length; r++) {
-			if (iCode == $scope.challenge[r].code) {
-				existsFlag = true;
-			} 
+		function checkForCode(title, cb) {
+			var titleRef = ref.child('challenge').child(title).once('value', function(snap) {
+				cb(snap.exists());
+			});
 		}
-		return existsFlag;
-	} 
 
-});
+	});
--- scripts/controllers/item.js
+++ scripts/controllers/item.js
@@ -5,38 +5,29 @@ app.controller('ItemController', function($scope, FURL, $firebase, $location, to
 	var ref = new Firebase(FURL);
 	var fbItems = $firebase(ref.child('items')).$asArray();
 
-	$scope.addItem = function(item) {	
+	$scope.addItem = function(item) {
 
-// var iItem = $scope.item.title;
+		var userId = $scope.item.title;
 
-var userId = $scope.item.title;
-  //checkIfUserExists(userId);
-ref.child('items').child(item.title).set({ title: item.title, points: item.points });
-alert('user ' + userId + ' does not exist!');
+		checkForTitle(item.title, function(doesExist) {
 
-		// 	fbItems.$add(item); 
-		// toaster.pop('success', "Item added");
-		// $scope.item = {title: '', points: ''};
+			if (doesExist) {
+				// already exists
+				toaster.pop('warning', "Item already exists");
+			} else {
+				// does not exist
+				ref.child('items').child(item.title).set({ title: item.title, points: item.points });
+				toaster.pop('success', "Item added");
+				$scope.item = {title: '', points: ''};
+			}
 
-		
-		
-			//$location.path('/admin/');
+		});
 	};
 
-function userExistsCallback(userId, exists) {
-  if (exists) {
-    alert('user ' + userId + ' exists!');
-  } else {
-  	ref.child('items').set({ item: 'title-2' });
-    alert('user ' + userId + ' does not exist!');
-  }
-}
-
-function checkIfUserExists(userId) {
-  ref.child('items').once('value', function(snapshot) {
-    var exists = (snapshot.val() !== null);
-    userExistsCallback(userId, exists);
-  });
-}
+	function checkForTitle(title, cb) {
+		var titleRef = ref.child('items').child(title).once('value', function(snap) {
+			cb(snap.exists());
+		});
+	}
 
 });
